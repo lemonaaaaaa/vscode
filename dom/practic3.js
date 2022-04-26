0242df00e15c7b1c907c3da06f3e37cd36846967
aaa <aaa@aaa.com>
2022-04-26 16:02:29 +0900

    first commit

---
+++ dom/practic3.js
@@ -0,0 +1,21 @@
+
+// let table = document.createElemen('table')
+// let thead = document.createElement('thead')
+// let tr = document.createElement('tr')
+// let tdoby = document.createElement('tbody')
+// let td = document.createElement('td')
+
+// <div id = "show">
+// <div>
+// <span id="name">홍길동</span>  -  <span class="age"> 20살 </span>
+// </div>
+//   </div>
+// <script>
+//   name.setAttribute('id', 'name');
+//   name.innerHTML = '홍길동'
+
+// </script> --내가쓴것들
+
+//사용하는 변수 선언
+
+let tavle, theadm, 
